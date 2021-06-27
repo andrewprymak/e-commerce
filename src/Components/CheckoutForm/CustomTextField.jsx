d5a38fb875208675dd95cc2c70a4bba7f5d95c1b
Andrew <andrewprymak86@gmail.com>
2021-06-27 12:53:02 +0300

    27/06

--- src/Components/CheckoutForm/CustomTextField.jsx
+++ src/Components/CheckoutForm/CustomTextField.jsx
@@ -2,20 +2,16 @@ import React from 'react';
 import { useFormContext, Controller } from 'react-hook-form';
 import { TextField, Grid } from '@material-ui/core';
 
-function FormInput({ name, label, required }) {
+function FormInput({ name, label }) {
   const { control } = useFormContext();
-  const isError = false;
+  // const isError = false;
 
   return (
     <Grid item xs={12} sm={6}>
-      <Controller
-        as={TextField}
-        name={name}
-        control={control}
-        label={label}
-        fullWidth
-        required={required}
-        error={isError}
+      <Controller 
+      render={({ field }) => <TextField {...field} label={label} fullWidth   required/>}
+      control={control}      
+      name={name}
       />
     </Grid>
   );
--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -3,11 +3,6 @@ import { Grid } from '@material-ui/core';
 import Product from './Product/Product';
 import useStyles from './styles';
 
-// const products = [
-//     { id: 1, name: 'Shoes', description: 'Running shoes', price: '$5', image: 'https://megasport.ua/ua/image/products/09_20211_bq3204_002_1_1612912321-68e7b68.jpg' },
-//     { id: 2, name: 'Macbook', description: 'Apple macbook', price: '$10', image: 'https://img.ktc.ua/img/base/1_505/6/316536.jpg' }
-// ]
-
 const Products = ( {products, onAddToCart} ) => {
     const classes = useStyles();
 
